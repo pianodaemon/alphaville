75895cd9b1e6034d72a9e1cb79b6f463f6d5cee9
ombdev <omontes.dev@gmail.com>
2021-08-10 18:23:18 -0500

    SSO: Added Auth User grpc (#6)
    * SSO: Added Auth User grpc
    
    * SSO: Delete automatically generated go files

--- DOS/sso/internal/storage/grpc_client.go
+++ DOS/sso/internal/storage/grpc_client.go
@@ -1,5 +1,38 @@
 package storage
 
+import (
+	"context"
+	"fmt"
+	"time"
+
+	pb "immortalcrab.com/sso/protos"
+
+	"google.golang.org/grpc"
+)
+
 func Authenticate(username, password string) (string, error) {
-	return "111", nil
+	// Set up a connection to the server.
+	conn, err := grpc.Dial("neon_nights:10080", grpc.WithInsecure())
+	if err != nil {
+		return "", fmt.Errorf("did not connect: %v", err)
+	}
+
+	defer conn.Close()
+	c := pb.NewValesClient(conn)
+
+	// Contact the server and print out its response.
+	ctx, cancel := context.WithTimeout(context.Background(), time.Second)
+	defer cancel()
+
+	r, err := c.AuthUser(ctx, &pb.UserCredentialsRequest{Username: username, Passwd: password})
+	if err != nil {
+		return "", fmt.Errorf("could not Auth User: %v", err)
+	}
+
+	rc := r.GetReturnCode()
+	if rc < 0 {
+		return "", fmt.Errorf("authentication failed for user %s: %s", username, r.GetReturnMessage())
+	}
+
+	return fmt.Sprintf("%d", rc), nil
 }

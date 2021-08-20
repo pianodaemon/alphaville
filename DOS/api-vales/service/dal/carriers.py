cf264b220fbf48970521089e781f0113b976d2df
Bob Ross <pianodaemon@gmail.com>
2021-08-20 08:27:47 -0500

    Update carriers.py

--- DOS/api-vales/service/dal/carriers.py
+++ DOS/api-vales/service/dal/carriers.py
@@ -1,5 +1,24 @@
+import math
 
+from misc.helperpg import run_stored_procedure, exec_steady, update_steady, EmptySetError
+from .entity import count_entities
 
+def alter_carrier(carrier_id, code, name):
+    """Calls database function in order to create/update a carrier"""
+
+    sql = """
+            SELECT * FROM alter_carrier(
+                {}::integer,
+                '{}'::character varying,
+                '{}'::character varying
+            ) AS (rc integer, msg text);
+            """.format(
+                carrier_id,
+                code.replace("'", "''"),
+                name.replace("'", "''")
+            )
+
+    return run_stored_procedure(sql)
 
 
 def delete_carrier(id):
@@ -18,7 +37,7 @@ def delete_carrier(id):
         msg = ''
     except EmptySetError as err:
         rc = -1
-        msg = 'Carrier with id {} does not exist.'.format(id)
+        msg = 'Patio with id {} does not exist.'.format(id)
     except Exception as err:
         rc = -1
         msg = repr(err)

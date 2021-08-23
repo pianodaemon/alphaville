ef8e28deeff4e1d30c9b102b2e22482c7ea34932
Bob Ross <pianodaemon@gmail.com>
2021-08-23 13:13:58 -0500

    voucher persistance interface (not ready yet)

---
+++ DOS/api-vales/service/dal/vouchers.py
@@ -0,0 +1,16 @@
+
+
+
+class VouchersPersistence(object):
+
+    @staticmethod
+    def alter(doc_id, carrier_id, patio_id, plat, obs):
+        pass
+
+    @staticmethod
+    def delete(doc_id):
+        pass
+
+    @staticmethod
+    def find_by(**kwargs):
+        pass

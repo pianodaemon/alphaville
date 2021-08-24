1b65fd2d8ff0f6e151c9746aeedc24bc2fe4bef2
Bob Ross <pianodaemon@gmail.com>
2021-08-23 23:38:21 -0500

    alter as of now is a class method

--- DOS/api-vales/service/dal/vouchers.py
+++ DOS/api-vales/service/dal/vouchers.py
@@ -4,10 +4,24 @@ from patios import get_patio
 
 class VouchersPersistence(object):
 
-    @staticmethod
-    def alter(doc_id, carrier_id, patio_id, plat, obs):
+    @classmethod
+    def alter(cls, doc_id, carrier_id, patio_id, plat, obs):
         pass
 
+    @staticmethod
+    def _create(col, carrier_id, patio_id, plat, obs):
+        """
+        It creates a newer voucher
+        within the collection
+        """
+        col.insert_one(
+            'platform': plat,
+            'observations': obs,
+            'carrier': get_carrier(carrier_id).get('clave', None),
+            'patio': get_patio(patio_id).get('clave', None),
+            'disabled': False
+        )
+    
     @staticmethod
     def delete(doc_id):
         pass
